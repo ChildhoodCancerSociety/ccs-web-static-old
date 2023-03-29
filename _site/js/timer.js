be55d9ddf914e938427679e587e8654cd045d434
Satoru Ouchi <sch.skouchi@gmail.com>
2023-03-29 11:49:21 -0700

    Let's try this again

--- _site/js/timer.js
+++ _site/js/timer.js
@@ -1,19 +1,19 @@
-var countDownDate = new Date("May 8, 2023 11:59:59").getTime();
+const countDownDate = new Date("May 8, 2023 11:59:59").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+const x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  const now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  const distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   document.getElementById("countDown").innerHTML = 
