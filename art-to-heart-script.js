7dbd8b111b80c35d5c00f3d4e10883afbdcadb94
Kyle Angelich <kyleangelich1@gmail.com>
2023-05-06 22:42:20 -0700

    Add documentation and revise syntax of javascript

--- art-to-heart-script.js
+++ art-to-heart-script.js
@@ -1,13 +1,15 @@
-function initiateCarousel() {
-  let elem = document.querySelector(".main-gallery");
-  let flkty = new Flickity( elem, {
+/**
+ * Initializes a Flickity carousel using the element with the class "main-gallery"
+ */
+const initiateCarousel = () => {
+  const elem = document.querySelector(".main-gallery");
+  const flkty = new Flickity(elem, {
     cellAlign: "center",
     contain: true,
     freeScroll: true,
     wrapAround: true,
     initialIndex: 1
   });
-}
-initiateCarousel();
+};
 
-// add documentation and arrow function notation
+initiateCarousel();
