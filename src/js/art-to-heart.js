dd3db2fdf1271bd2eebe519ed1889009b95cd298
Kyle Angelich <kyleangelich1@gmail.com>
2023-05-07 19:05:14 -0700

    Add a bunch of broken 11ty files

---
+++ src/js/art-to-heart.js
@@ -0,0 +1,15 @@
+/**
+ * Initializes a Flickity carousel using the element with the class "main-gallery"
+ */
+const initiateCarousel = () => {
+    const elem = document.querySelector(".main-gallery");
+    const flkty = new Flickity(elem, {
+      cellAlign: "center",
+      contain: true,
+      freeScroll: true,
+      wrapAround: true,
+      initialIndex: 1
+    });
+  };
+  
+  initiateCarousel();
