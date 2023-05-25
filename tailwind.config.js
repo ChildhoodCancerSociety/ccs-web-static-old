10ee0b0b997960a91c803de537a004369ea1208f
Kyle Angelich <kyleangelich1@gmail.com>
2023-05-25 00:48:36 -0700

    Push broken tailwind files and config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // presets: [
+  //   require("@ccs-dev/tailwind")
+  // ],
+  content: ["src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
