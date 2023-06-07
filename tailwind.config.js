f8627e89cc8ce3178779674fe1362ebc1c266518
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-07 00:20:05 -0700

    Convert hero section to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ["./_site/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      sizing: {
+        width: {
+          "30rem": "30rem",
+        },
+        height: {
+          "30rem": "30rem",
+        },
+      },
+
+      colors: {
+        primary: "var(--primary-ccs-color)",
+        secondary: "var(--secondary-ccs-color)",
+        tertiary: "var(--tertiary-ccs-color)",
+        quaternary: "var(--quaternary-ccs-color)",
+      },
+
+      font: {
+        header: "var(--font-header-nunito)",
+        body: "var(--font-body-quicksand)",
+      },
+    },
   },
   plugins: [],
 }
-
