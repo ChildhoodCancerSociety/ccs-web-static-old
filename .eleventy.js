b2b52e0700a5ff648ed8cee29b757335fcdd21e2
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-06 18:11:56 -0700

    Modify 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,27 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/assets/");
-    eleventyConfig.addPassthroughCopy("src/css/");
-    eleventyConfig.addPassthroughCopy("src/js/");
-    eleventyConfig.addWatchTarget("src/css/");
-    eleventyConfig.addWatchTarget("src/js/");
-    return {
-      dir: {
-        input: 'src',
-        includes: '_includes',
-        output: '_site',
-      },
-      templateFormats: ['md', 'njk', 'html'],
-      markdownTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-    };
-  }
+  // Copies all files in selected directories directly to the output file
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addPassthroughCopy("src/js/");
+  // Could we use this instead?
+  // eleventyConfig.addPassthroughCopy("src/*/");
+
+  // 11ty will watch the selected directories and will automatically rebuild the site with any changes
+  eleventyConfig.addWatchTarget("src/assets/");
+  eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addWatchTarget("src/js/");
+  // Could we use this instead?
+  // eleventyConfig.addWatchTarget("src/*/");
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
