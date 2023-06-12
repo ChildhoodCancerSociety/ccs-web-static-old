0c611cf685cca802f4bd54a0fb49a4dd3392099f
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-12 12:11:59 -0700

    Convert header to tailwindcss

--- src/js/art-to-heart.js
+++ src/js/art-to-heart.js
@@ -2,14 +2,14 @@
  * Initializes a Flickity carousel using the element with the class "main-gallery"
  */
 const initiateCarousel = () => {
-    const elem = document.querySelector(".main-gallery");
-    const flkty = new Flickity(elem, {
-      cellAlign: "center",
-      contain: true,
-      freeScroll: true,
-      wrapAround: true,
-      initialIndex: 1
-    });
-  };
+  const elem = document.querySelector(".main-gallery");
+  const flkty = new Flickity(elem, {
+    cellAlign: "center",
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    initialIndex: 1
+  });
+};
   
   initiateCarousel();
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,11 @@
 function mobileMenu() {
-    const menu = document.getElementById("mobileNav");
-    const icon = document.getElementById("menuIcon");
-    if (icon.getAttribute('src') == "./assets/img/icons/menu-icon-white.png") {
-      icon.src = "./assets/img/close-icon-white.png";
-      menu.style.left = '0';
-    } else {
-      icon.src = "./assets/img/icons/menu-icon-white.png";
-      menu.style.left = '-100%';
-    }
+  const menu = document.getElementById("mobileNav");
+  const icon = document.getElementById("menuIcon");
+  if (icon.getAttribute('src') == "./assets/img/icons/menu-icon-white.png") {
+    icon.src = "./assets/img/close-icon-white.png";
+    menu.style.left = '0';
+  } else {
+    icon.src = "./assets/img/icons/menu-icon-white.png";
+    menu.style.left = '-100%';
   }
+}
