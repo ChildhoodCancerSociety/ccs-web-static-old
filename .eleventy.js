a7dbef3b44b9a24d54d6d4fec9a611273492dd7f
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-14 13:00:02 -0700

    Configure ted-scholars page for 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -11,15 +11,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addPassthroughCopy("src/js/");
-  // Could we use this instead?
-  // eleventyConfig.addPassthroughCopy("src/*/");
 
   // 11ty will watch the selected directories and will automatically rebuild the site with any changes
   eleventyConfig.addWatchTarget("src/assets/");
   eleventyConfig.addWatchTarget("src/css/");
   eleventyConfig.addWatchTarget("src/js/");
-  // Could we use this instead?
-  // eleventyConfig.addWatchTarget("src/*/");
+
   return {
     dir: {
       input: 'src',
