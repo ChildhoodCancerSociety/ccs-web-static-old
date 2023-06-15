81942bcd3e883fbfeeb18fda54c3f470e368326b
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-15 15:28:42 -0700

    Revert entire branch to pre-tailwind

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,11 @@
-/*
-* Toggles the visibilty of the mobile-menu and changes the menu icon
-*/
-const mobileMenu = () => {
-  const menu = document.getElementById("mobileNav");
-  const icon = document.getElementById("menuIcon");
-  
-  if (icon.getAttribute('src') === "./assets/img/icons/menu-icon-white.png") {
-    icon.src = "./assets/img/close-icon-white.png";
-    menu.style.left = '0';
-  } else {
-    icon.src = "./assets/img/icons/menu-icon-white.png";
-    menu.style.left = '-100%';
+function mobileMenu() {
+    const menu = document.getElementById("mobileNav");
+    const icon = document.getElementById("menuIcon");
+    if (icon.getAttribute('src') == "./assets/img/icons/menu-icon-white.png") {
+      icon.src = "./assets/img/close-icon-white.png";
+      menu.style.left = '0';
+    } else {
+      icon.src = "./assets/img/icons/menu-icon-white.png";
+      menu.style.left = '-100%';
+    }
   }
-};
