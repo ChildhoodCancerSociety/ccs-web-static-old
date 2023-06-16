eb1f0e279b6b5c9115f3dab6cc64ab1b579a21dd
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-15 18:59:59 -0700

    Implement applicable post-tailwind edits

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 module.exports = function(eleventyConfig) {
+  const now = Date.now().toString();
+  eleventyConfig.addWatchTarget('./tailwind.config.js')
+  eleventyConfig.addWatchTarget('./tailwind.css')
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  eleventyConfig.addShortcode('version', function () {
+    return now;
+  })
 
   // Copies all files in selected directories directly to the output file
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addPassthroughCopy("src/js/");
-  // Could we use this instead?
-  // eleventyConfig.addPassthroughCopy("src/*/");
 
   // 11ty will watch the selected directories and will automatically rebuild the site with any changes
   eleventyConfig.addWatchTarget("src/assets/");
   eleventyConfig.addWatchTarget("src/css/");
   eleventyConfig.addWatchTarget("src/js/");
-  // Could we use this instead?
-  // eleventyConfig.addWatchTarget("src/*/");
   return {
     dir: {
       input: 'src',
