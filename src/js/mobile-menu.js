feed6a9374f3cbb8f2f833c7c005f6beb7f89328
Kyle Angelich <kyleangelich1@gmail.com>
2023-06-16 13:35:50 -0700

    Fix images pathing for mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,11 +5,11 @@ const mobileMenu = () => {
   const menu = document.getElementById("mobileNav");
   const icon = document.getElementById("menuIcon");
 
-  if (icon.getAttribute('src') === "./assets/img/icons/menu-icon-white.png") {
-    icon.src = "./assets/img/icon/close-icon-white.png";
+  if (icon.getAttribute('src') === "/assets/img/icons/menu-icon-white.png") {
+    icon.src = "/assets/img/icons/close-icon-white.png";
     menu.style.left = '0';
   } else {
-    icon.src = "./assets/img/icons/menu-icon-white.png";
+    icon.src = "/assets/img/icons/menu-icon-white.png";
     menu.style.left = '-100%';
   }
 };
